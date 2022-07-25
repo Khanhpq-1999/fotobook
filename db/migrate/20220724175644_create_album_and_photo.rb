4121bad44c4687548408598d108b8ed0c6bc14ce
Khanhpq-1999 <109057402+Khanhpq-1999@users.noreply.github.com>
2022-07-25 09:09:54 +0700

    Update 20220724175644_create_album_and_photo.rb
    add space for right syntax and change status column type to boolean

--- db/migrate/20220724175644_create_album_and_photo.rb
+++ db/migrate/20220724175644_create_album_and_photo.rb
@@ -1,19 +1,19 @@
 class CreateAlbumAndPhoto < ActiveRecord::Migration[7.0]
   def change
     create_table :albums do |t|
-      t.string :title, limit:140
-      t.string :description, limit:300
-      t.string :status, limit:15
+      t.string :title, limit: 140
+      t.string :description, limit: 300
+      t.boolean :status
       t.timestamps
     end
 
     add_reference :albums, :users
 
     create_table :photos do |t|
-      t.string :url, limit:255
+      t.string :url, limit: 255
       t.string :title, limit: 140
-      t.string :description, limit:300
-      t.string :status, limit:15
+      t.string :description, limit: 300
+      t.boolean :status
       t.belongs_to :albums
       t.timestamps
     end
